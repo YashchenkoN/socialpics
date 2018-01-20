b68393bc5669ef0559f10f8d572e508165690d1f
kolyan <vkont4@gmail.com>
2018-01-20 20:38:57 +0200

    [Java 9] Added module-info.java for all modules

---
+++ sp-facebook/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module io.github.yashchenkon.socialpics.discovery {
+    requires spring.boot;
+    requires spring.web;
+    requires spring.boot.autoconfigure;
+    requires spring.cloud.commons;
+    requires spring.cloud.netflix.eureka.server;
+    requires spring.context;
+}
---
+++ sp-gateway/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module io.github.yashchenkon.socialpics.discovery {
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.cloud.netflix.zuul;
+}
